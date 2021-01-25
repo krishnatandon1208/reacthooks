c5502dce7432f342ba22d4b8141cca064a52ad39
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-01-25 13:22:31 +0530

    increment and decrement using hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ counter, setCounter ] = useState(0);
+
+	const increaseCounterHandler = () => {
+		setCounter(counter + 1);
+	};
+
+	const decreaseCounterHandler = () => {
+		setCounter(counter - 1);
+	};
+
+	return (
+		<div className="App">
+			<header>Understanding REACT HOOKS</header>
+			<div>
+				<p> Current Counter Value = {counter} </p>
+				<button onClick={increaseCounterHandler}>Increase</button>
+				<button onClick={decreaseCounterHandler}>Decrease</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
